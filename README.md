f20a96b086c77f7f3acce6cff3f308832ed7ee32
Poundhound <k.norizo@gmail.com>
2014-03-30 09:33:59 +0900

    getTime()で経過ミリ秒数求まるのでコードを簡素化

--- README.md
+++ README.md
@@ -2,9 +2,9 @@
 現在の日付からn日前までの記事にNEWアイコンをつけるjQueryのプラグインです。
 
 ##使い方
-jQueryを呼び出した後に$.fn.addNewIcon()を呼び出してください。  
-HTMLのマークアップはdl>dt+ddの形式になります。 (サンプルコードindex.htmlをご覧ください) 
-dtの日付(YYYY-MM-DD)を参照して初期値3日前の記事にNewアイコンをつけます。  
+jQueryを呼び出した後に$.fn.addNewIcon()を呼び出してください。
+HTMLのマークアップはdl>dt+ddの形式になります。 (サンプルコードindex.htmlをご覧ください)
+dtの日付(YYYY-MM-DD)を参照して初期値3日前の記事にNewアイコンをつけます。
 
 	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.8/jquery.min.js"></script>
 	<script src="js/jq.addnew.js"></script>
@@ -15,13 +15,15 @@ dtの日付(YYYY-MM-DD)を参照して初期値3日前の記事にNewアイコ�
 	</script>
 
 ##オプション
-distanceは何日前の記事までNEWアイコンをつけるか(初期値:3)、targetClassはdlのクラス名をそれぞれ変更出来ます(初期値:.box__info)。
+distanceは何日前の記事までNEWアイコンをつけるか(初期値:3)、targetClassはdlのクラス名(初期値:.box\__info)、iconClassはアイコンのクラス名(初期値:icon__new)、elemは追加する要素(初期値:i)を指定。
 
 	$(function() {
 		$.fn.addNewIcon({
 			distance: 5, //5日前の記事までアイコン付加
-			targetClass: '.whats-new' //クラス名は.whats-new
+			targetClass: '.whats-new', //クラス名は.whats-new
+			iconClass: 'icon-new', //クラス名は.icon-new
+			elem: 'span' //span要素を追加する
 		});
 	});
 
-日付の形式を変更する場合は31行目を変更。
+日付の形式を変更する場合は28行目を変更。（YYYY/MM/DDなら('/'))
--- js/jq.addnew.js
+++ js/jq.addnew.js
@@ -6,47 +6,38 @@
  */
 
 $.fn.addNewIcon = function(options) {
-	
+
 	var o = $.extend({
-		distance: 3,  //NEWアイコンをつける期間
-		targetClass: '.box__info' //クラス
+		distance: 3,  // NEWアイコンをつける期間
+		targetClass: '.box__info', // ターゲットのクラス名
+		iconClass: 'icon__new', // アイコンのクラス名
+		elem: 'i'
 	}, options);
 
 	$('dt', o.targetClass).each(function() {
-
 		//期間を経過ミリ秒数に変換
-		var dt = o.distance * 86400000;
+		var distanceTime = o.distance * 86400000;
 
-		//今日の日付を取得
+		// 今日のDateオブジェクトを生成
 		var today = new Date();
-		var y = today.getFullYear();
-		var m = today.getMonth() + 1;
-		var d = today.getDate();
-		//今日を経過ミリ秒数に変換
-		var ct = convertDate(y, m, d);
+		// 今日を経過ミリ秒数で求める
+		var baseTime = today.getTime();
 
-		//dtに記述された日付を取得
+		// dtに記述された日付を取得
 		var textVal = $(this).text();
-		//-で分割し、配列にセット
+		// -で分割し、配列にセット
 		var dateArr = textVal.split('-');
-		//dtに記述された日付を経過ミリ秒数に変換
-		var td = convertDate(dateArr[0], dateArr[1], dateArr[2]);
-		
-		//dtに記述された日付が今日から何日前か求める
-		var dist = ct - td;
-		//distanceで設定した期間内であればNEWアイコンを付加
-		if(dist <= dt) {
-		 	$(this).next('dd').append('<span class="new">NEW</span>');
+		// dtに記述された日付でDateオブジェクトを生成
+		var entryDay = new Date(dateArr[0], dateArr[1]-1, dateArr[2]);
+		// 経過ミリ秒数に変換
+		var entryTime = entryDay.getTime();
+
+		// dtに記述された日付(経過ミリ秒数)が今日から何日前(経過ミリ秒数)かを求める
+		var dist = baseTime - entryTime;
+		// distanceで設定した期間内であればNEWアイコンを付加
+		if(dist <= distanceTime) {
+		 	$(this).next('dd').append('<' + o.elem + ' class="' + o.iconClass + '">NEW</' + o.elem + '>');
 		}
-
-		//年月日を経過ミリ秒に変換
-		function convertDate(year, month, day) {
-			var sd = new Date(year, month - 1, day);
-			var cd = sd.getTime(); //経過ミリ秒数に変換
-			return cd; //日付を返す
-		}
-
-
 
 	});
 	return this;
